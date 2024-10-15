bb73c3ba4bd0750b58169edf845e0dbc8a6208df
tkemmer <tkemmer@users.noreply.github.com>
2024-10-15 11:16:42 +0000

    deploy: 395e7e67599f7b7d9480efbdc91eaf226aea9b00

---
+++ v0.0.0/trait.impl/mscore/timstof/collision/trait.TimsTofCollisionEnergy.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"mscore":[],
+"rustdf":[["impl TimsTofCollisionEnergy for <a class=\"struct\" href=\"rustdf/sim/dia/struct.TimsTofSyntheticsFrameBuilderDIA.html\" title=\"struct rustdf::sim::dia::TimsTofSyntheticsFrameBuilderDIA\">TimsTofSyntheticsFrameBuilderDIA</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
